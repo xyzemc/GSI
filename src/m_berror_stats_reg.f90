90b088b93ed84cbd1a53a9c9e394b730c1148fa7
donald.e.lippi@noaa.gov <donald.e.lippi@noaa.gov>
2017-05-12 18:18:42 +0000

    dl2rw_2: default corz, hwll, vz

--- src/m_berror_stats_reg.f90
+++ src/m_berror_stats_reg.f90
@@ -358,9 +358,8 @@ end subroutine berror_read_bal_reg
   real(r_kind) :: raux
 
   ! corz = sqrt(corz)
-  !real(r_kind), parameter :: corz_default=one*0.10000,hwll_default=500.00000000,&!100000_r_kind,&
-  !                           vz_default=one*60.00
-  real(r_kind), parameter :: corz_default=one*0.100000,hwll_default=500.00000000,vz_default=one*60.00
+  real(r_kind), parameter :: corz_default=one,hwll_default=100000_r_kind,vz_default=one
+  !real(r_kind), parameter :: corz_default=one,hwll_default=27000.00000000,vz_default=one*10
 
   allocate ( clat_avn(mlat) )
   allocate ( sigma_avn(1:msig) )
