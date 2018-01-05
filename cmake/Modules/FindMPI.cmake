0b8cdb29e675fa82befa132059fb1ecc7073e527
Mark A Potts <Mark.Potts@noaa.gov>
2018-01-05 13:52:39 +0000

    Added in cmake files from version 3.10 of cmake that should work with older versions

---
+++ cmake/Modules/FindMPI.cmake
@@ -0,0 +1,12 @@
+# This extends CMake's FindHDF5.cmake to add support to include MPI include
+# paths and libraries in the HDF5 ones if HDF5_IS_PARALLEL is ON
+# (BUG #0014363).
+
+# include the default FindHDF5.cmake.
+if(CMAKE_VERSION VERSION_LESS 3.10)
+  message("hey, including new FindMPI")
+  include(${CMAKE_CURRENT_LIST_DIR}/NewCMake/FindMPI.cmake)
+else()
+  include(${CMAKE_ROOT}/Modules/FindMPI.cmake)
+endif()
+
