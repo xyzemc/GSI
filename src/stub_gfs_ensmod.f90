04cdc582303d085d76910acde4b70e00852b8ec8
Mark A. Potts <mark.potts@noaa.gov>
2018-01-23 16:11:26 +0000

    Fixed missing (:) which was causing compile to fail
    Change-Id: Ie46bff4ce4a6af6ba343cdafba94d55adbb9f2a1

--- src/stub_gfs_ensmod.f90
+++ src/stub_gfs_ensmod.f90
@@ -1,9 +1,6 @@
 module get_gfs_ensmod_mod
 
-    use mpeu_util, only: die
-    use mpimod, only: mype,npe
     use abstract_get_gfs_ensmod_mod
-
     implicit none
 
     type, extends(abstract_get_gfs_ensmod_class) :: get_gfs_ensmod_class
@@ -15,140 +12,65 @@ module get_gfs_ensmod_mod
 
 contains
 
-subroutine get_user_ens_dummy(this,grd,ntindex,atm_bundle,iret)
-!$$$  subprogram documentation block
-!                .      .    .                                       .
-! subprogram:    get_user_ens_    pretend atmos bkg is the ensemble
-!   prgmmr: mahajan          org: emc/ncep            date: 2016-06-30
-!
-! abstract: Read in GFS ensemble members in to GSI ensemble.
-!
-! program history log:
-!   2016-06-30  mahajan  - initial code
-!   2016-07-20  mpotts   - refactored into class/module
-!
-!   input argument list:
-!     grd      - grd info for ensemble
-!     member   - index for ensemble member
-!     ntindex  - time index for ensemble
-!
-!   output argument list:
-!     atm_bundle - atm bundle w/ fields for ensemble member
-!     iret       - return code, 0 for successful read.
-!
-! attributes:
-!   language: f90
-!   machine:  ibm RS/6000 SP
-!
-!$$$
 
-    use kinds, only: i_kind,r_kind,r_single
-    use gridmod, only: use_gfs_nemsio
-    use general_sub2grid_mod, only: sub2grid_info
-    use hybrid_ensemble_parameters, only: n_ens,ens_fast_read
-    use hybrid_ensemble_parameters, only: grd_ens
-    use gsi_bundlemod, only: gsi_bundle
-    use control_vectors, only: nc2d,nc3d
+  subroutine get_user_ens_dummy(this,grd,ntindex,atm_bundle,iret)
 
-    implicit none
+     use kinds, only: i_kind
+     use general_sub2grid_mod, only: sub2grid_info
+     use gsi_bundlemod, only: gsi_bundle
 
-    ! Declare passed variables
-    class(get_gfs_ensmod_class), intent(inout) :: this
-    type(sub2grid_info), intent(in   ) :: grd
-    integer(i_kind),     intent(in   ) :: ntindex
-    type(gsi_bundle),    intent(inout) :: atm_bundle(:)
-    integer(i_kind),     intent(  out) :: iret
+     implicit none
 
-    ! Declare internal variables
-    character(len=*),parameter :: myname_='get_user_ens_gfs'
-    real(r_single),allocatable,dimension(:,:,:,:) :: en_loc3
-    integer(i_kind) :: m_cvars2d(nc2d),m_cvars3d(nc3d)
+     ! Declare passed variables
+     class(get_gfs_ensmod_class), intent(inout) :: this
+     type(sub2grid_info), intent(in   ) :: grd
+     integer(i_kind),     intent(in   ) :: ntindex
+     type(gsi_bundle),    intent(inout) :: atm_bundle(:)
+     integer(i_kind),     intent(  out) :: iret
+     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
+     end associate
+     iret = 0
+ 
+     return
 
-    integer(i_kind) :: n
-    real(r_kind),allocatable,dimension(:) :: clons,slons
+  end subroutine get_user_ens_dummy
+  
+  subroutine put_gsi_ens_dummy(this,grd,member,ntindex,atm_bundle,iret)
 
-    associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
-    end associate
+     use kinds, only: i_kind
+     use general_sub2grid_mod, only: sub2grid_info
+     use gsi_bundlemod, only: gsi_bundle
 
-    return
+     implicit none
+     ! Declare passed variables
+     class(get_gfs_ensmod_class), intent(inout) :: this
+     type(sub2grid_info), intent(in   ) :: grd
+     integer(i_kind),     intent(in   ) :: member
+     integer(i_kind),     intent(in   ) :: ntindex
+     type(gsi_bundle),    intent(inout) :: atm_bundle
+     integer(i_kind),     intent(  out) :: iret
 
-end subroutine get_user_ens_dummy
+     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
+     end associate
+     iret = 0
+     return
+  end subroutine put_gsi_ens_dummy
 
-subroutine put_gsi_ens_dummy(this,grd,member,ntindex,atm_bundle,iret)
-!$$$  subprogram documentation block
-!                .      .    .                                       .
-! subprogram:    put_gsi_ens_    write out an internally gen ens to file
-!   prgmmr: mahajan          org: emc/ncep            date: 2016-06-30
-!
-! abstract: Write out GSI ensemble to file.
-!
-! program history log:
-!   2016-06-30  mahajan  - initial code
-!   2016-07-20  mpotts   - refactored into class/module
-!
-!   input argument list:
-!     grd      - grd info for ensemble
-!     member   - index for ensemble member
-!     ntindex  - time index for ensemble
-!   atm_bundle - bundle of ensemble perturbations
-!
-!   output argument list:
-!     iret      - return code, 0 for successful write
-!
-! attributes:
-!   language: f90
-!   machine:  ibm RS/6000 SP
-!
-!$$$
+  subroutine non_gaussian_ens_grid_dummy(this,elats,elons)
 
-    use kinds, only: i_kind
-    use general_sub2grid_mod, only: sub2grid_info
-    use gsi_bundlemod, only: gsi_bundle
-    use gsi_4dvar, only: ens_fhrlevs
-    use hybrid_ensemble_parameters, only: ensemble_path
-    use hybrid_ensemble_parameters, only: sp_ens
-    use gridmod, only: use_gfs_nemsio
+     use kinds, only: r_kind
+     use hybrid_ensemble_parameters, only: sp_ens
+     implicit none
+     ! Declare passed variables
+     class(get_gfs_ensmod_class), intent(inout) :: this
+     real(r_kind), intent(out) :: elats(size(sp_ens%rlats)),elons(size(sp_ens%rlons))
 
-    implicit none
+     elats=sp_ens%rlats
+     elons=sp_ens%rlons
+     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
+     end associate
 
-    ! Declare passed variables
-    class(get_gfs_ensmod_class), intent(inout) :: this
-    type(sub2grid_info), intent(in   ) :: grd
-    integer(i_kind),     intent(in   ) :: member
-    integer(i_kind),     intent(in   ) :: ntindex
-    type(gsi_bundle),    intent(inout) :: atm_bundle
-    integer(i_kind),     intent(  out) :: iret
-
-    ! Declare internal variables
-    character(len=*),parameter :: myname_='put_gsi_ens_gfs'
-    character(len=70) :: filename
-    integer(i_kind) :: mype_atm
-    logical,save :: inithead = .true.
-
-    associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
-    end associate
-    mype_atm = member
-
-    return
-
-end subroutine put_gsi_ens_dummy
-
-subroutine non_gaussian_ens_grid_dummy(this,elats,elons)
-
-    use kinds, only: r_kind
-    use hybrid_ensemble_parameters, only: sp_ens
-
-    implicit none
-
-    ! Declare passed variables
-    class(get_gfs_ensmod_class), intent(inout) :: this
-    real(r_kind), intent(out) :: elats(size(sp_ens%rlats)),elons(size(sp_ens%rlons))
-
-    associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
-    end associate
-
-    return
-
-end subroutine non_gaussian_ens_grid_dummy
+     return
+  end subroutine non_gaussian_ens_grid_dummy
 
 end module get_gfs_ensmod_mod
