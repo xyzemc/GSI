34ee4bef1943bc7d82dbf707a567fc05a56c758c
Mark A. Potts <mark.potts@noaa.gov>
2018-01-23 15:48:43 +0000

    Removed extra stuff
    Change-Id: I4a9344717aa45f9d4b17917e70d6020fa1b27abf

--- src/stub_gfs_ensmod.f90
+++ src/stub_gfs_ensmod.f90
@@ -70,23 +70,6 @@ subroutine get_user_ens_dummy(this,grd,ntindex,atm_bundle,iret)
     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
     end associate
 
-    if ( use_gfs_nemsio .and. ens_fast_read ) then
-       allocate(en_loc3(grd_ens%lat2,grd_ens%lon2,nc2d+nc3d*grd_ens%nsig,n_ens))
-       allocate(clons(grd_ens%nlon),slons(grd_ens%nlon))
-       call get_user_ens_gfs_fastread_(ntindex,en_loc3,m_cvars2d,m_cvars3d, &
-                         grd_ens%lat2,grd_ens%lon2,grd_ens%nsig, &
-                         nc2d,nc3d,n_ens,iret,clons,slons)
-       do n=1,n_ens
-          call move2bundle_(grd,en_loc3(:,:,:,n),atm_bundle(n), &
-                            m_cvars2d,m_cvars3d,iret,clons,slons)
-       end do
-       deallocate(en_loc3,clons,slons)
-    else
-       do n = 1,n_ens
-          call get_user_ens_gfs_member_(grd,n,ntindex,atm_bundle(n),iret)
-       end do
-    endif
-
     return
 
 end subroutine get_user_ens_dummy
@@ -146,29 +129,6 @@ subroutine put_gsi_ens_dummy(this,grd,member,ntindex,atm_bundle,iret)
     end associate
     mype_atm = member
 
-    write(filename,13) trim(adjustl(ensemble_path)),ens_fhrlevs(ntindex),member
-13  format(a,'sigf',i2.2,'_ens_pert',i3.3)
-
-    if ( use_gfs_nemsio ) then
-       if ( mype == 0 ) then
-          write(6,*) 'write_nemsatm is not adapted to write out perturbations yet'
-          iret = 999
-       endif
-       !call write_nemsatm(grd,...)
-    else
-       call general_write_gfsatm(grd,sp_ens,sp_ens,filename,mype_atm, &
-            atm_bundle,ntindex,inithead,iret)
-    endif
-
-    inithead = .false.
-
-    if ( iret /= 0 ) then
-       if ( mype == mype_atm ) then
-          write(6,'(A)') 'put_gsi_ens_: WARNING!'
-          write(6,'(3A,I5)') 'Trouble writing ensemble perturbation to file : ', trim(filename), ', IRET = ', iret
-       endif
-    endif
-
     return
 
 end subroutine put_gsi_ens_dummy
@@ -186,8 +146,6 @@ subroutine non_gaussian_ens_grid_dummy(this,elats,elons)
 
     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
     end associate
-    elats=sp_ens%rlats
-    elons=sp_ens%rlons
 
     return
 
