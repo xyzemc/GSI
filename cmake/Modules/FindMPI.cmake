16f2c05238f9ca1a11215be09251d64117996122
Mark A Potts <Mark.Potts@noaa.gov>
2018-02-09 18:59:13 +0000

    Added checks for version of cmake to imports from future cmake attempts

--- cmake/Modules/FindMPI.cmake
+++ cmake/Modules/FindMPI.cmake
@@ -3,8 +3,11 @@
 # (BUG #0014363).
 
 # include the default FindHDF5.cmake.
-if(CMAKE_VERSION VERSION_LESS 3.10)
+if(CMAKE_VERSION VERSION_LESS 3.1)
+  include(${CMAKE_ROOT}/Modules/FindMPI.cmake)
+elseif(CMAKE_VERSION VERSION_LESS 3.10)
   message("hey, including new FindMPI")
+  message("hey, cmake version is ${CMAKE_VERSION}")
   include(${CMAKE_CURRENT_LIST_DIR}/NewCMake/FindMPI.cmake)
 else()
   include(${CMAKE_ROOT}/Modules/FindMPI.cmake)
