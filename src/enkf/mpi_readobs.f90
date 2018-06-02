e88e493e735b5927ff45c148ea663c242e322b43
jswhit <Jeffrey.S.Whitaker@noaa.gov>
2018-06-02 21:36:03 +0000

    take out debug prints

--- src/enkf/mpi_readobs.f90
+++ src/enkf/mpi_readobs.f90
@@ -267,9 +267,7 @@ subroutine mpi_getobs(obspath, datestring, nobs_conv, nobs_oz, nobs_sat, nobs_to
 
     if (nproc == 0) t1 = mpi_wtime()
     call mpi_bcast(ensmean_ob,nobs_tot,mpi_real4,0,mpi_comm_world,ierr)
-    print *, nproc, 'after first bcast'
     call mpi_bcast(sprd_ob,nobs_tot,mpi_real4,0,mpi_comm_world,ierr)
-    print *, nproc, 'after 2nd bcast'
     if (nproc == 0) then
         t2 = mpi_wtime()
         print *,'time to broadcast ob prior ensemble mean and spread = ',t2-t1
