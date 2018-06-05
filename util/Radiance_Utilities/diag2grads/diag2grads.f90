21513600a0353d1862e031bf12f9a6dd27feb332
Mingjing.Tong <mingjing.tong@noaa.gov>
2018-06-05 09:11:11 -0400

    Add averging and subsampling functions in diag2grads

--- util/Radiance_Utilities/diag2grads/diag2grads.f90
+++ util/Radiance_Utilities/diag2grads/diag2grads.f90
@@ -40,6 +40,8 @@ program diag2grads
 ! Declare namelist with defaults
   logical           :: retrieval        = .false.
   logical           :: lwrite_stn       = .true.
+  logical           :: subsample        = .false.
+  logical           :: laverage         = .false.
 
   integer           :: idsat            = 14   ! HIRS14
   integer           :: n_chan           = 19
@@ -54,6 +56,7 @@ program diag2grads
   real(4)           :: South            = -90. ! Statistics area
   real(4)           :: West             =   0. ! Statistics area
   real(4)           :: East             = 360. ! Statistics area
+  real(4)           :: meshsize         = 1.0  ! subsample or average over meshbin(degree) x meshbin(degree) box
 
   integer           :: qc               =   0  ! 0:all 1:only qc pass -1:only rejected
   integer           :: solar            =   0  ! 0:all 1:night 2:day
@@ -63,14 +66,17 @@ program diag2grads
   
   character(len=GRADS_MAXLEN_COMMENT)  :: comment  = ''
   character(len=GRADS_MAXLEN_FILENAME) :: filename = ''
+  character(len=GRADS_MAXLEN_FILENAME) :: filename2 = ''
   character(len=GRADS_MAXLEN_FILENAME+4) :: dsetname = ''
   character(len=GRADS_MAXLEN_FILENAME+9) :: headname = ''
   character(len=GRADS_MAXLEN_FILENAME+8) :: fixname = ''
   character(len=GRADS_MAXLEN_FILENAME+9) :: channame = ''
+  character(len=GRADS_MAXLEN_FILENAME+8) :: sbsname = ''
+  character(len=GRADS_MAXLEN_FILENAME+8) :: avgname = ''
   
   namelist / PARM / comment, ft_diag_top, ft_diag_end, filename, &
        idsat, n_chan, datetime, tint, South, North, West, East, &
-       qc, solar, retrieval, lwrite_stn
+       qc, solar, retrieval, lwrite_stn, subsample, laverage, meshsize
   
 
 
@@ -82,12 +88,22 @@ program diag2grads
   type(diag_data_chan_list  ),allocatable :: data_chan(:)
   type(diag_data_extra_list ),allocatable :: data_extra(:,:)
   
+! Subsample or bin averge variables
+  integer :: mx,my
+  integer,allocatable :: imesh(:,:,:) 
+  real(4),allocatable,dimension(:) :: glon,glat
+  real(4),allocatable,dimension(:) :: slon,slat 
+  real(4),allocatable,dimension(:,:) :: wgt
+  real(4),allocatable,dimension(:,:,:) :: sbsval
+  real(4),allocatable,dimension(:,:,:) :: avgval
   real(4),allocatable :: nuchan(:)
   real(4),allocatable :: data_sfc(:)
   real(4),allocatable :: data_lvl(:,:)
+  integer :: ixm, iym
+  real(4) :: twgt
+  logical :: lselect
 
   character(len=GRADS_LEN_ID) :: id
-
 
 ! Other variables
   character(8):: stid
@@ -100,9 +116,9 @@ program diag2grads
   integer :: time_work(5)
   
   integer :: iobs_total
-  integer :: i, n, k
+  integer :: i, j, n, k
   integer :: npred_radiag, n_bcor, nfix, nfout
-  integer :: lunstn,lunctl,lunhead,lunfix,lunchan
+  integer :: lunstn,lunctl,lunhead,lunfix,lunchan,lunsbs,lunavg,lunsbsctl
   integer :: nlev,nflag,itime_terminator
 
   real(4):: rlat,rlon,rtim
@@ -113,6 +129,9 @@ program diag2grads
   lunhead = 23
   lunfix = 24
   lunchan = 25
+  lunsbs = 26
+  lunavg = 27
+  lunsbsctl=28
 
   nfix=3
   nfout=8
@@ -157,7 +176,37 @@ program diag2grads
   headname = trim(filename) // '_head.dat'
   fixname  = trim(filename) // '_fix.dat'
   channame = trim(filename) // '_chan.dat'
-  
+  sbsname = trim(filename) // '_sbs.dat'
+  avgname = trim(filename) // '_avg.dat'
+
+  if (subsample .or. laverage) then
+     mx = 360.0 / meshsize
+     my = 180.0 / meshsize
+     allocate(glon(mx),glat(my))
+     allocate(imesh(mx,my,n_chan))
+     allocate(wgt(mx,my))
+     do i = 1, mx
+       glon(i) = meshsize*(i-0.5)
+     end do
+     do j = 1, my
+       glat(j) = -90.0 + meshsize*(j-0.5)
+     end do
+     imesh = 0
+     wgt = 0.0
+  end if
+!  print *,'mx, my=', mx, my
+
+  if (subsample)then 
+     allocate(slon(mx),slat(my))
+     allocate(sbsval(mx,my,n_chan))
+     slon=GRADS_MISSING
+     slat=GRADS_MISSING
+     sbsval=GRADS_MISSING
+  end if
+  if (laverage)then
+     allocate(avgval(mx,my,n_chan))
+     avgval=GRADS_MISSING
+  end if
   
   if(lwrite_stn)then
      open(lunstn,file=dsetname,form='unformatted')
@@ -166,6 +215,12 @@ program diag2grads
   open(lunhead,file=headname,form='unformatted',access='stream')
   open(lunfix,file=fixname,form='unformatted',access='stream')
   open(lunchan,file=channame,form='unformatted',access='stream')
+  if(subsample)then
+     open(lunsbs,file=sbsname,form='unformatted')
+  end if
+  if(laverage)then
+     open(lunavg,file=avgname,form='unformatted',access='stream')
+  end if
 
 ! Allocate channel list array
   allocate( nuchan(n_chan) )
@@ -318,6 +373,21 @@ program diag2grads
         data_sfc(22) = data_fix%sfc_wndspd
         data_sfc(23) = data_fix%qcdiag1
         data_sfc(24) = data_fix%qcdiag2
+
+!     Compute observation grid index and weight in subsample/averge mesh
+        ixm = nint(rlon/meshsize + 0.5)
+        iym = nint((rlat+90.0)/meshsize + 0.5)
+        twgt = (rlat-glat(iym))*(rlat-glat(iym))+(rlon-glon(ixm))*(rlon-glon(ixm))
+        twgt = (meshsize * meshsize * 0.5) / twgt
+!        print *,'rlon, ixm, rlat, iym=', rlon, ixm, rlat, iym
+        lselect = .false.
+        if (subsample .and. twgt > wgt(ixm,iym)) then
+           slon(ixm) = rlon
+           slat(iym) = rlat
+           wgt(ixm,iym) = twgt
+           lselect = .true.
+        end if
+
         
 !     Initialize the channel (level) data
         do n=1,n_chan
@@ -329,7 +399,7 @@ program diag2grads
 !     Loop over channels
         iexist = 0
         do n = 1, n_chan
-      
+
 !          Check Tb
            if( qc /= -2 .and. ( abs(data_chan(n)%omgnbc) > 200. .or. &
                 data_chan(n)%tbobs < 50. .or. data_chan(n)%tbobs > 500. ) )cycle
@@ -340,7 +410,7 @@ program diag2grads
            
 !          There is data to be written
            iexist = 1
-        
+
 !          Load arrays with level (channel) dependent data
            data_lvl(1,n)  = data_chan(n)%tbobs
            data_lvl(2,n)  = data_chan(n)%omgbc
@@ -419,12 +489,23 @@ program diag2grads
               data_lvl(16+header_fix%angord+2,n) = data_chan(n)%bisst
               data_lvl(16+header_fix%angord+3,n) = data_chan(n)%sprd
            endif
-           
+
+!       subsample cloud effect
+           if (subsample .and. lselect) then
+              sbsval(ixm,iym,n) = data_lvl(8,n)
+           end if
+
+!       average OmF with BC
+           if(laverage)then
+              if (data_lvl(2,n) /= GRADS_MISSING) then
+                 if (imesh(ixm,iym,n) == 0) avgval(ixm,iym,n) = 0.0
+                 imesh(ixm,iym,n)=imesh(ixm,iym,n)+1
+                 avgval(ixm,iym,n) = avgval(ixm,iym,n) + data_lvl(2,n)
+              end if
+           end if
+
 !       End loop over channels
         enddo
-
-        write(6,*)'TCC=', (data_lvl(7,n),n=1,n_chan)
-
 
 !       Check if data should be written
         if( iexist == 0 )cycle
@@ -433,7 +514,7 @@ program diag2grads
 
         write(lunfix) data_sfc(9),data_sfc(23),data_sfc(24)
         write(lunchan)(nuchan(k),(data_lvl(i,k),i=1,nfout),k=1,n_chan)
-      
+
 !       Write station data
         if(lwrite_stn)call write_station_data(lunstn,&
                            id, &                         ! id
@@ -459,8 +540,7 @@ program diag2grads
 
 !    End loop over data records        
      enddo
-     
-    
+
 !    Write a record to indicate the end of time group
      if (lwrite_stn .and. itime_terminator==0) then
         write(6,*)'Write GrADS station data terminator for time ft_diag=',ft_diag
@@ -476,7 +556,19 @@ program diag2grads
 
 ! End loop over times     
   enddo
-    
+
+! Average OmF with BC
+  if(laverage)then
+     do n=1,n_chan
+        do j=1,my
+           do i=1,mx
+              if (imesh(i,j,n) /= 0)then
+                  avgval(i,j,n)=avgval(i,j,n)/imesh(i,j,n)
+              end if
+           end do
+        end do
+     end do
+  end if
     
 ! Close GrADS station file
   if (lwrite_stn .and. itime_terminator==0) then
@@ -512,7 +604,61 @@ program diag2grads
           data_name)
      close(lunctl)
   endif
-  
+
+! subsample cloud effect (sbsval(i,j,1) /= 0.0 only for cloud effect)
+  if (subsample) then
+     rtim=0.0
+     do j=1,my
+        do i=1,mx
+           if (slon(i) /= GRADS_MISSING .and. slat(j) /= GRADS_MISSING &
+               .and. sbsval(i,j,1) /= GRADS_MISSING .and. sbsval(i,j,1) /= 0.0) then 
+              call write_station_lvldata(lunsbs,&
+                                         id, &   ! id
+                                         slat(j), & ! lat
+                                         slon(i), & ! lon
+                                         rtim, & ! t
+                                         n_chan, &  ! nlev(# of chan)
+                                         1, &
+                                         nuchan, &   ! ch # assigned as hight
+                                         sbsval(i,j,:))
+           end if
+        end do
+     end do
+
+     write(6,*)'Write GrADS station data terminator'
+     stid='end'
+     rlat=0.0
+     rlon=0.0
+     rtim=0.0
+     nlev=0
+     nflag=0
+     write(lunsbs) stid,rlat,rlon,rtim,nlev,nflag
+
+     dsetname = trim(filename) // '_sbs.ctl'
+     filename2 = trim(filename)//'_sbs'
+     open(lunsbsctl,file=dsetname,form='formatted')
+     call write_station_lvlctl(lunsbsctl,comment, &
+          filename2, &
+          datetime, &
+          tint, &
+          1, &
+          n_chan, &
+          1, &
+          'sbscldeff ')
+     close(lunsbsctl)
+  end if
+
+  if (laverage) then
+     write(lunavg) real(mx,4),real(my,4),real(n_chan,4)
+     write(lunavg) glon
+     write(lunavg) glat
+     do n=1,n_chan
+        write(lunavg) avgval(:,:,n)
+     end do
+  end if
+
+  close(lunsbs)
+  close(lunavg)
 
 ! Check total number of data
   if( iobs_total <= 0 )then
--- util/Radiance_Utilities/diag2grads/write_station.f90
+++ util/Radiance_Utilities/diag2grads/write_station.f90
@@ -19,6 +19,23 @@ contains
     
     return
   end subroutine write_station_data
+
+  subroutine write_station_lvldata(lunstn,stid,rlat,rlon,rtim,n_chan,nelem_lv,nuchan,data_lv)
+    implicit none
+
+    character(8):: stid
+    integer:: lunstn,n_chan,nelem_lv
+    real(4),dimension(n_chan):: nuchan
+    real(4):: rlat,rlon,rtim
+    real(4),dimension(nelem_lv,n_chan):: data_lv
+    integer:: nflag,i,k
+
+    nflag=1
+    write(lunstn) stid,rlat,rlon,rtim,n_chan+1,nflag
+    write(lunstn) (nuchan(k),(data_lv(i,k),i=1,nelem_lv),k=1,n_chan)
+
+    return
+  end subroutine write_station_lvldata
   
 
   subroutine write_station_ctl(lunctl,comment,filename,datetime,tint,nt,n_chan,nelem_lvl,nelem_sfc,data_name)
@@ -75,5 +92,58 @@ contains
     write(lunctl,'("endvars")')
     
   end subroutine write_station_ctl
+
+  subroutine write_station_lvlctl(lunctl,comment,filename,datetime,tint,nt,n_chan,nelem_lvl,data_name)
+    use grads_hdr
+    use read_diag
+    implicit none
+
+    integer :: lunctl
+    character(len=GRADS_MAXLEN_COMMENT)  :: comment
+    character(len=GRADS_MAXLEN_FILENAME) :: filename
+    integer :: datetime(5)
+    integer :: tint, nt,i
+    integer :: n_chan,nelem_lvl
+    character(len=10):: data_name(nelem_lvl)
+    character(len=GRADS_MAXLEN_FILENAME+4):: dsetname
+    character(len=3),dimension(12):: cmonth
+    character(len=15):: string
+    integer :: imissing
+
+    data cmonth / "Jan", "Feb", "Mar", "Apr", "May", "Jun", &
+         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" /
+
+    print *,'filename=', trim(filename)
+
+    dsetname=trim(filename) // '.dat'
+    if (dsetname(1:1) == '/') then
+       write(lunctl,'("dset ",a)') trim(dsetname)
+    else
+       write(lunctl,'("dset ^",a)') trim(dsetname)
+    endif
+    write(lunctl,'("dtype station")')
+
+    dsetname=trim(filename) // '.map'
+    if (dsetname(1:1) == '/') then
+       write(lunctl,'("stnmap ",a)') trim(dsetname)
+    else
+       write(lunctl,'("stnmap ^",a)') trim(dsetname)
+    endif
+
+    write(lunctl,'("options sequential big_endian")')
+    imissing = grads_missing
+    write(lunctl,'("undef ",i6)') imissing
+    write(lunctl,'("title ",a)') trim(comment)
+
+    write(string,'(i2.2,":",i2.2,"Z",i2.2,a3,i4.4)') datetime(4),datetime(5),datetime(3),cmonth(datetime(2)),datetime(1)
+    write(lunctl,'("tdef ",i3," linear ",a,1x,i2,"hr")') nt,string,tint
+    write(lunctl,'("vars ",i3)') nelem_lvl
+    do i=1,nelem_lvl
+       write(lunctl,'(a,1x,i6,(" 99 "),a)') trim(data_name(i)),n_chan,trim(data_name(i))
+    end do
+
+    write(lunctl,'("endvars")')
+
+  end subroutine write_station_lvlctl
 
 end module write_station
