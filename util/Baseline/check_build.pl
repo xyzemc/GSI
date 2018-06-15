7270ff9f20e206dc9989c0498a33284b80059b23
Mark A Potts <Mark.Potts@noaa.gov>
2018-06-15 15:43:13 +0000

    Added script to check on baseline builds

---
+++ util/Baseline/check_build.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+  $baselineDir = "/gpfs/dell2/emc/modeling/noscrub/Mark.Potts/GSI-Baseline";
+  chdir ($baselineDir);
+  $hashcmd = 'git log --pretty=oneline | head -1 | awk \'{print $1}\'';
+  $lasthash = `$hashcmd`;
+  chop($lasthash);
+  $updatecmd = "git remote update; git pull origin DA-FV3-IMPL-merged";
+  system("$updatecmd");
+  $newhash = `$hashcmd`;
+  chop($newhash);
+
+# if($newhash ne $lasthash) { 
+  if($newhash eq $lasthash) { 
+    # rebuild new master    
+    # this is just for mars until it gets added to the master
+#   $sedcmd = 'sed -i \'s/v\\[0-9/m\\[0-9/g\' CMakeLists.txt';
+    system($sedcmd);
+    $buildcmd = "./ush/build_all_cmake.sh $baselineDir 0";
+#   system($buildcmd); 
+    chdir ("$baselineDir/build");
+    system("REND=2 ctest -j 14");
+  }
+
+
