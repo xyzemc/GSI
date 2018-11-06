acdaa48f6f012df61580a8896537c3a769930acc
david.groff <David.Groff@noaa.gov>
2018-11-06 16:44:00 +0000

    Deleting local build of GSI that was erroneously committed and pushed to redmine.

--- build_efsoi/libsrc/ncdiag/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /scratch4/NCEPDEV/da/save/David.Groff/David.Groff/version_revised_fv3efsoi/ProdGSI/libsrc/ncdiag
-# Build directory: /scratch4/NCEPDEV/da/save/David.Groff/David.Groff/version_revised_fv3efsoi/ProdGSI/build_efsoi/libsrc/ncdiag
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
