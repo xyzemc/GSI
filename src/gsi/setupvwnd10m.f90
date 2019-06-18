b862ce00edfa8d6210cf8842d595734fe96e210f
Steven Levine <steven.levine@noaa.gov>
2019-06-18 21:45:42 +0000

    allow for mesonet wind sensor height to be read in and processed.  Copied in code block from setupw.f90

--- src/gsi/setupvwnd10m.f90
+++ src/gsi/setupvwnd10m.f90
@@ -22,6 +22,7 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
 !   2017-09-28  todling - add netcdf_diag capability; hidden as contained code
 !   2018-01-08  pondeca - addd option l_closeobs to use closest obs to analysis
 !                                     time in analysis
+!   2019-06-18  levine  - read in geometric wind height (m above ground) for surface mesonets
 !
 !   input argument list:
 !     lunin    - unit from which to read observations
@@ -38,11 +39,12 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
 !   machine:  ibm RS/6000 SP
 !
 !$$$
-  use mpeu_util, only: die,perr
+  use mpeu_util, only: die,perr,getindex
+  use state_vectors, only: svars3d, levels, nsdim
   use kinds, only: r_kind,r_single,r_double,i_kind
 
   use guess_grids, only: hrdifsig,nfldsig,ges_lnprsl, &
-               sfcmod_gfs,sfcmod_mm5,comp_fact10,pt_ll     
+               sfcmod_gfs,sfcmod_mm5,comp_fact10,pt_ll,geop_hgtl     
   use m_obsdiags, only: vwnd10mhead
   use obsmod, only: rmiss_single,i_vwnd10m_ob_type,obsdiags, bmiss, &
                     lobsdiagsave,nobskeep,lobsdiag_allocated,time_offset
@@ -62,7 +64,7 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
   use constants, only: zero,tiny_r_kind,one,one_tenth,half,wgtlim,rd,grav,&
             two,cg_term,three,four,five,ten,huge_single,r1000,r3600,&
             grav_ratio,flattening,grav,deg2rad,grav_equator,somigliana, &
-            semi_major_axis
+            semi_major_axis,eccentricity
   use jfunc, only: jiter,last,miter
   use qcmod, only: dfact,dfact1,npres_print,qc_satwnds
   use convinfo, only: nconvtype,cermin,cermax,cgross,cvar_b,cvar_pg,ictype
@@ -71,6 +73,7 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
   use gsi_bundlemod, only : gsi_bundlegetpointer
   use gsi_metguess_mod, only : gsi_metguess_get,gsi_metguess_bundle
   use rapidrefresh_cldsurf_mod, only: l_closeobs
+  use sparsearr, only: sparr2
   implicit none
 
 ! Declare passed variables
@@ -103,28 +106,32 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
   real(r_kind) obserrlm,obserror,val,valqc,dx10,rlow,rhgh,drpx,prsfc
   real(r_kind) term,rwgt
   real(r_kind) cg_vwnd10m,wgross,wnotgross,wgt,arg,exp_arg,rat_err2,qcgross
-  real(r_kind) presw,factw,dpres,sfcchk,ugesin,vgesin,dpressave
+  real(r_kind) presw,factw,dpres,sfcchk,ugesin,vgesin,dpressave,fact
   real(r_kind) qcu,qcv
-  real(r_kind) ratio_errors,tfact,wflate,psges,goverrd,spdob
-  real(r_kind) uob,vob,spdb
+  real(r_kind) ratio_errors,tfact,wflate,psges,goverrd,spdob,zsges
+  real(r_kind) uob,vob,slat,sin2,termg,termr,termrg,pobl
+  real(r_kind) spdb,dstn
   real(r_kind) dudiff_opp, dvdiff_opp, vecdiff, vecdiff_opp
   real(r_kind) ascat_vec
   real(r_kind) errinv_input,errinv_adjst,errinv_final
   real(r_kind) err_input,err_adjst,err_final,skint,sfcr
   real(r_kind) uob_reg,vob_reg,uob_e,vob_e,dlon_e,uges_e,vges_e,dudiff_e,dvdiff_e
+  real(r_kind) dz,zob,z1,z2,p1,p2,dz21,dlnp21,defrough
   real(r_kind),dimension(nobs):: dup
-  real(r_kind),dimension(nsig)::prsltmp,tges
+  real(r_kind),dimension(nsig)::prsltmp,tges,zges
   real(r_kind) wdirob,wdirgesin,wdirdiffmax
   real(r_kind),dimension(nele,nobs):: data
+  real(r_kind) :: delz
   real(r_single),allocatable,dimension(:,:)::rdiagbuf
 
 
   integer(i_kind) ier,ier2,ilon,ilat,ihgt,iuob,ivob,ipres,id,itime,ikx,iqc
-  integer(i_kind) iuse,ilate,ilone,ielev,izz,iprvd,isprvd
+  integer(i_kind) iuse,ilate,ilone,ielev,izz,iprvd,isprvd,iz,u_ind,v_ind
   integer(i_kind) i,nchar,nreal,k,ii,ikxx,nn,isli,ibin,ioff,ioff0,jj,itype
   integer(i_kind) l,mm1
   integer(i_kind) istat
   integer(i_kind) idomsfc,iskint,iff10,isfcr
+  integer(i_kind) k1,k2
   
   logical,dimension(nobs):: luse,muse
   integer(i_kind),dimension(nobs):: ioid ! initial (pre-distribution) obs ID
@@ -136,8 +143,11 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
   character(8),allocatable,dimension(:):: cprvstg,csprvstg
   character(8) c_prvstg,c_sprvstg
   real(r_double) r_prvstg,r_sprvstg
+  logical::sfc_data
 
-  logical:: in_curbin, in_anybin
+  type(sparr2) :: dhx_dx_u, dhx_dx_v
+  real(r_single), dimension(nsdim) :: dhx_dx_array
+  logical:: in_curbin,in_anybin,save_jacobian
   integer(i_kind),dimension(nobs_bins) :: n_alloc
   integer(i_kind),dimension(nobs_bins) :: m_alloc
   class(obsNode   ), pointer:: my_node
@@ -364,27 +374,224 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
 
      itype=ictype(ikx)
 
+!    Process observations reported with height differently than those
+!    reported with pressure.  Type 223=profiler and 224=vadwnd are 
+!    encoded in NCEP prepbufr files with geometric height above 
+!    sea level.  Type 229=pibal profiler is reported using 
+!    geopotenital height.  Some type 221=pibal wind observations are
+!    also repoted using geopotential height.
+
+     sfc_data = (itype >=280 .and. itype < 300)!LEVINE .and. (.not.twodvar_regional)
+     if (sfc_data) then
+
+        drpx = zero
+        dpres = data(ihgt,i)
+        dstn = data(ielev,i)
+        call tintrp2a11(ges_z,zsges,dlat,dlon,dtime,hrdifsig,&
+             mype,nfldsig)
+!       Subtract off combination of surface station elevation and
+!       model elevation depending on how close to surface
+        fact = zero
+        !LEVINE EDIT
+        if (itype == 295 .or. itype == 280) then
+           !if ((dpres-dstn) < 10._r_kind) then
+           !   print*, "SETUPW-MSONET: Mesonet wind with height under 10 m:",dpres,dstn
+           !endif
+        endif
+        if(dpres-dstn > 10._r_kind)then
+           if(dpres-dstn > r1000)then
+              fact = one
+           else
+              fact=(dpres-dstn)/990._r_kind
+           end if
+        end if
+        dpres=dpres-(dstn+fact*(zsges-dstn))
+        if(itype==261) dpres = data(ihgt,i)
+
+!       Get guess surface elevation and geopotential height profile 
+!       at observation location.
+        call tintrp2a1(geop_hgtl,zges,dlat,dlon,dtime,hrdifsig,&
+             nsig,mype,nfldsig)
+
+!       For observation reported with geometric height above sea level,
+!       convert geopotential to geometric height.
+
+        if ((itype>=223 .and. itype<=228) .or. sfc_data) then
+!          Convert geopotential height at layer midpoints to geometric 
+!          height using equations (17, 20, 23) in MJ Mahoney's note 
+!          "A discussion of various measures of altitude" (2001).  
+!          Available on the web at
+!          http://mtp.jpl.nasa.gov/notes/altitude/altitude.html
+!
+!          termg  = equation 17
+!          termr  = equation 21
+!          termrg = first term in the denominator of equation 23
+!          zges  = equation 23
+
+           slat = data(ilate,i)*deg2rad
+           sin2  = sin(slat)*sin(slat)
+           termg = grav_equator * &
+                ((one+somigliana*sin2)/sqrt(one-eccentricity*eccentricity*sin2))
+           termr = semi_major_axis /(one + flattening + grav_ratio -  &
+                two*flattening*sin2)
+           termrg = (termg/grav)*termr
+           do k=1,nsig
+              zges(k) = (termr*zges(k)) / (termrg-zges(k))  ! eq (23)
+           end do
+ 
+        endif
+
+!       Given observation height, (1) adjust 10 meter wind factor if
+!       necessary, (2) convert height to grid relative units, (3) compute
+!       compute observation pressure (for diagnostic purposes only), and
+!       (4) compute location of midpoint of first model layer above surface
+!       in grid relative units
+
+!       Adjust 10m wind factor if necessary.  Rarely do we have a
+!       profiler/vad obs within 10 meters of the surface.  Almost always,
+!       the code below resets the 10m wind factor to 1.0 (i.e., no
+!       reduction in wind speed due to surface friction).
+
+!       Convert observation height (in dpres) from meters to grid relative
+!       units.  Save the observation height in zob for later use.
+        zob = dpres
+        call grdcrd1(dpres,zges,nsig,1)
+
+!       Interpolate guess u and v to observation location and time.
+ 
+        call tintrp31(ges_uwnd10m,ugesin,dlat,dlon,dpres,dtime, &
+           hrdifsig,mype,nfldsig)
+        call tintrp31(ges_vwnd10m,vgesin,dlat,dlon,dpres,dtime, &
+           hrdifsig,mype,nfldsig)
+
+        iz = max(1, min( int(dpres), nsig))
+        delz = max(zero, min(dpres - float(iz), one))
+
+        if (save_jacobian) then
+
+           u_ind = getindex(svars3d, 'u10m')
+           if (u_ind < 0) then
+              print *, 'Error: no variable u in state vector. Exiting.'
+              call stop2(1300)
+           endif
+           v_ind = getindex(svars3d, 'v10m')
+           if (v_ind < 0) then
+              print *, 'Error: no variable v in state vector. Exiting.'
+              call stop2(1300)
+           endif
+
+           dhx_dx_u%st_ind(1)  = iz               + sum(levels(1:u_ind-1))
+           dhx_dx_u%end_ind(1) = min(iz + 1,nsig) + sum(levels(1:u_ind-1))
+           dhx_dx_v%st_ind(1)  = iz               + sum(levels(1:v_ind-1))
+           dhx_dx_v%end_ind(1) = min(iz + 1,nsig) + sum(levels(1:v_ind-1))
+
+           dhx_dx_u%val(1) = one - delz
+           dhx_dx_u%val(2) = delz
+           dhx_dx_v%val = dhx_dx_u%val
+        endif
+
+
+        if (zob > zges(1).and..not.twodvar_regional) then
+           if (itype.eq.295.or.itype.eq.288) then
+              print*, "Setting factw to 1! stnid,zob,zges(1)=",station_id,zob,zges(1)
+           endif
+           factw=one
+        else
+           !if (itype.eq.295.or.itype.eq.288) then
+              !print*, "Factw will change!  stnid,zob,zges(1)=",station_id,zob,zges(1)
+           !endif
+           !factw = data(iff10,i)
+           factw=one !LEVINE default value of factw
+           if(sfcmod_gfs .or. sfcmod_mm5) then
+              sfcr = data(isfcr,i)
+              skint = data(iskint,i)
+              call comp_fact10(dlat,dlon,dtime,skint,sfcr,isli,mype,factw)
+           end if
+
+           if (zob <= ten) then
+              if (zob <= zero) then
+                 print*, "WARNING: Negative ZOB for station,zob,type (correcting to 10 m):",station_id,zob,itype
+                 zob=ten
+              endif
+              if(zob < ten)then
+                 !term = max(zob,zero)/ten
+                 !factw = term*factw
+                 defrough=half !default roughness length: 0.5 m
+                 factw=log(zob/defrough)/log(ten/defrough)
+              end if
+           else
+              term = (zges(1)-zob)/(zges(1)-ten)
+              factw = one-term+factw*term
+           end if
+
+           !if (factw.ne.1) then
+           !   print*, "StationID,zob,zges(1),factw=",station_id,zob,zges(1),factw
+           !endif
+           ugesin=factw*ugesin
+           vgesin=factw*vgesin
+
+           if (save_jacobian) then
+              dhx_dx_u%val = factw * dhx_dx_u%val
+              dhx_dx_v%val = factw * dhx_dx_v%val
+           endif
+        endif
+
+        if(sfc_data .or. dpres < one) then
+           drpx=0.005_r_kind*abs(dstn-zsges)*(one-fact)
+        end if
+ 
+!       Compute observation pressure (only used for diagnostics)
+
+!       Set indices of model levels below (k1) and above (k2) observation.
+        if (dpres<one) then
+           z1=zero;    p1=log(psges)
+           z2=zges(1); p2=prsltmp(1)
+        elseif (dpres>nsig) then
+           z1=zges(nsig-1); p1=prsltmp(nsig-1)
+           z2=zges(nsig);   p2=prsltmp(nsig)
+           drpx = 1.e6_r_kind
+        else
+           k=dpres
+           k1=min(max(1,k),nsig)
+           k2=max(1,min(k+1,nsig))
+           z1=zges(k1); p1=prsltmp(k1)
+           z2=zges(k2); p2=prsltmp(k2)
+        endif
+       
+        dz21     = z2-z1
+        dlnp21   = p2-p1
+        dz       = zob-z1
+        pobl     = p1 + (dlnp21/dz21)*dz
+        presw    = ten*exp(pobl)
+
+!       Determine location in terms of grid units for midpoint of
+!       first layer above surface
+        sfcchk=zero
+!       call grdcrd1(sfcchk,zges,nsig,1)
+
+
 !    Process observations with reported pressure
+     else
         dpres = data(ipres,i)
         presw = ten*exp(dpres)
         dpres = dpres-log(psges)
         drpx=zero
-      
+        
         prsfc=psges
         prsln2=log(exp(prsltmp(1))/prsfc)
         dpressave=dpres
-
-!       Put obs pressure in correct units to get grid coord. number
+        
+        !       Put obs pressure in correct units to get grid coord. number
         dpres=log(exp(dpres)*prsfc)
         call grdcrd1(dpres,prsltmp(1),nsig,-1)
- 
-!       Interpolate guess u and v to observation location and time.
- 
+        
+        !       Interpolate guess u and v to observation location and time.
+        
         call tintrp2a11(ges_uwnd10m,ugesin,dlat,dlon,dtime,hrdifsig,&
              mype,nfldsig)
         call tintrp2a11(ges_vwnd10m,vgesin,dlat,dlon,dtime,hrdifsig,&
              mype,nfldsig)
-
+        
         if(dpressave <= prsln2)then
            factw=one
         else
@@ -394,10 +601,10 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
               skint = data(iskint,i)
               call comp_fact10(dlat,dlon,dtime,skint,sfcr,isli,mype,factw)
            end if
- 
+           
            call tintrp2a1(ges_tv,tges,dlat,dlon,dtime,hrdifsig,&
-              nsig,mype,nfldsig)
-!          Apply 10-meter wind reduction factor to guess winds
+                nsig,mype,nfldsig)
+           !          Apply 10-meter wind reduction factor to guess winds
            dx10=-goverrd*ten/tges(1)
            if (dpressave < dx10)then
               term=(prsln2-dpressave)/(prsln2-dx10)
@@ -405,13 +612,14 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
            end if
            ugesin=factw*ugesin   
            vgesin=factw*vgesin
- 
+           
         end if
-       
-!       Get approx k value of sfc by using surface pressure
+        
+        !       Get approx k value of sfc by using surface pressure
         sfcchk=log(psges)
         call grdcrd1(sfcchk,prsltmp(1),nsig,-1)
-
+     endif
+     
 !    Checks based on observation location relative to model surface and top
      rlow=max(sfcchk-dpres,zero)
      rhgh=max(dpres-r0_001-rsigp,zero)
@@ -1094,4 +1302,3 @@ subroutine setupvwnd10m(lunin,mype,bwork,awork,nele,nobs,is,conv_diagsave)
   end subroutine final_vars_
 
 end subroutine setupvwnd10m
-
