8fc6c8b17b71a53f5d90c1801dda42609cd278b6
guoqing.ge <Guoqing.Ge@noaa.gov>
2019-11-06 14:06:26 -0700

    updated FindGRIB2.cmake for GSI-CAPS

--- cmake/Modules/FindGRIB2.cmake
+++ cmake/Modules/FindGRIB2.cmake
@@ -1,21 +1,21 @@
 # - Find the GRIB2 modules
 set( NO_DEFAULT_PATH )
 find_library( G2_LIB4  
-    NAMES libg2_4.a
+    NAMES libg2.a
     HINTS
-       $ENV{GRIB2PATH}/g2/v2.5.0/lib 
+       /scratch1/BMC/wrfruc/ejames/wrf_code/GSD_UPP/comupp/lib
     ${NO_DEFAULT_PATH})
 
 find_library( G2TMPL_LIB
     NAMES libg2tmpl.a
     HINTS
-       $ENV{GRIB2PATH}/g2tmpl/v1.3.0/lib 
+       /scratch1/BMC/wrfruc/ejames/wrf_code/GSD_UPP/comupp/lib
     ${NO_DEFAULT_PATH})
 
 find_path( GRIB2INC
     NAMES grib_mod.mod
     HINTS
-       $ENV{GRIB2PATH}/g2/v2.5.0/incmod/g2_4 
+       /scratch1/BMC/wrfruc/ejames/wrf_code/GSD_UPP/comupp/include
     ${NO_DEFAULT_PATH})
 
 set( GRIB2_LIBRARY ${G2_LIB4} ${G2TMPL_LIB} CACHE STRING "GRIB2 Library Location" )
@@ -24,23 +24,23 @@ set( GRIB2_INCLUDE_PATH ${GRIB2INC} CACHE STRING "GRIB2 Include Location" )
 # - Find the JASPER modules
 set( NO_DEFAULT_PATH )
 find_library( JASPER_LIB
-    NAMES libjasper.a
+    NAMES libjasper.so
     HINTS
-       $ENV{GRIB2PATH}/jasper/v1.900.1/lib
+       /usr/lib64
     ${NO_DEFAULT_PATH})
 
 # - Find the PNG modules
 set( NO_DEFAULT_PATH )
 find_library( PNG_LIB
-    NAMES libpng.a
+    NAMES libpng.so
     HINTS
-       $ENV{GRIB2PATH}/libpng/v1.6.34/lib
+       /usr/lib64
     ${NO_DEFAULT_PATH})
 
 # - Find the Z modules
 set( NO_DEFAULT_PATH )
 find_library( Z_LIB
-    NAMES libz.a
+    NAMES libz.so
     HINTS
-       $ENV{GRIB2PATH}/zlib/v1.2.6/lib
+       /usr/lib64
     ${NO_DEFAULT_PATH})
