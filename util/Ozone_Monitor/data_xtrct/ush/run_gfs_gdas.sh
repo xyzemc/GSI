09d37b51024de97bb647f723dc381cc82bbf4842
edward.safford <edward.safford@noaa.gov>
2020-01-06 14:22:41 +0000

    Ref #66694
    Update with changes for wcoss_c.
    
    Change-Id: I647928acc07f6cf583eaadfd6c1ed1dc8852da10

--- util/Ozone_Monitor/data_xtrct/ush/run_gfs_gdas.sh
+++ util/Ozone_Monitor/data_xtrct/ush/run_gfs_gdas.sh
@@ -8,7 +8,7 @@ run=gdas
 NET=gfs
 envir=prod
 
-MY_MACHINE=wcoss_d
+MY_MACHINE=cray
 package="ProdGSI/util/Ozone_Monitor"
 #package="OznMon"
 
@@ -32,7 +32,7 @@ elif [[ $MY_MACHINE = "wcoss_d" ]]; then
 fi
 
 
-if [[ $MY_MACHINE = "theia" ]]; then
+if [[ $MY_MACHINE = "hera" ]]; then
    scripts=/scratch4/NCEPDEV/da/noscrub/Edward.Safford/ProdGSI/util/Ozone_Monitor/data_xtrct/ush
 elif [[ $MY_MACHINE = "wcoss" ]]; then
    scripts=/gpfs/gd2/emc/da/noscrub/Edward.Safford/${package}/data_xtrct/ush
@@ -42,12 +42,12 @@ elif [[ $MY_MACHINE = "cray" ]]; then
    scripts=/gpfs/hps3/emc/da/noscrub/Edward.Safford/ProdGSI/util/Ozone_Monitor/data_xtrct/ush
 fi
 
-idate=`${scripts}/find_cycle.pl -dir ~/nbns/stats/${OZN_SUFFIX} -cyc 1 -run ${run}`
-echo "idate = $idate"
+#idate=`${scripts}/find_cycle.pl -dir ~/nbns/stats/${OZN_SUFFIX} -cyc 1 -run ${run}`
+#echo "idate = $idate"
 
-export NDATE=/gpfs/dell1/nco/ops/nwprod/prod_util.v1.1.1/exec/ips/ndate
-#export START_DATE=2019082906
-START_DATE=`${NDATE} +06 $idate`
+#export NDATE=/gpfs/dell1/nco/ops/nwprod/prod_util.v1.1.1/exec/ips/ndate
+export START_DATE=2020010500
+#START_DATE=`${NDATE} +06 $idate`
 
 PDY=`echo $START_DATE | cut -c1-8`
 cyc=`echo $START_DATE | cut -c9-10`
@@ -61,12 +61,12 @@ export oznstat=${COM_IN}/${run}.t${cyc}z.oznstat
 export OZN_TANKDIR=/u/${LOGNAME}/nbns
 #export OZN_TANKDIR=/home/${LOGNAME}/nbns
 
-log=/gpfs/dell2/ptmp/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.log
+#log=/gpfs/dell2/ptmp/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.log
 #log=/ptmpd1/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.log
-#log=./log
+log=./log
 
 #err=/ptmpd1/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.err
-err=/gpfs/dell2/ptmp/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.err
-#err=./err
+#err=/gpfs/dell2/ptmp/Edward.Safford/logs/${OZN_SUFFIX}/${run}/oznmon/OznMon_DE.err
+err=./err
 
 ${scripts}/OznMon_DE.sh $OZN_SUFFIX -p $START_DATE -r gdas 1>$log 2>$err
