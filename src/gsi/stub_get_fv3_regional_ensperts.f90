808c9686e5e0852afcd13083b8a174a81cd1e5d3
Jing Guo <jing.guo@nasa.gov>
2020-02-12 13:59:59 -0500

    gmaodev/jguo/refactOO+PGIfixes: cleaned up m_extOzone.F90. added stub_get_fv3_regional_ensperts.f90
    Change-Id: Ibc4ab396ecc49d72781ed69d6ed71f69e28212d8

--- src/gsi/stub_get_fv3_regional_ensperts.f90
+++ src/gsi/stub_get_fv3_regional_ensperts.f90
@@ -1,6 +1,36 @@
 module get_fv3_regional_ensperts_mod
-use abstract_get_fv3_regional_ensperts_mod,only: abstract_get_fv3_regional_ensperts_class
+!$$$  subprogram documentation block
+!                .      .    .                                       .
+! subprogram:	 module stub_get_fv3_regional_ensperts
+!   prgmmr:	 j guo <jing.guo-1@nasa.gov>
+!      org:	 NASA/GSFC, Global Modeling and Assimilation Office, 610.3
+!     date:	 2020-02-12
+!
+! abstract: a stub of a type(get_fv3_regional_ensperts_class) object
+!
+! program history log:
+!   2020-02-12  j guo   - tailored from cplr_get_fv3_regional_ensperts.f90, to
+!                         support builds without a need for an actual object
+!                         of type(get_fv3_regional_ensperts_class).
+!
+!   input argument list: see Fortran 90 style document below
+!
+!   output argument list: see Fortran 90 style document below
+!
+! attributes:
+!   language: Fortran 90 and/or above
+!   machine:
+!
+!$$$  end subprogram documentation block
+
+! module interface:
+
+  use abstract_get_fv3_regional_ensperts_mod,only: abstract_get_fv3_regional_ensperts_class
   use kinds, only : i_kind
+  implicit none
+  private	! except
+  public :: get_fv3_regional_ensperts_class
+
   type, extends(abstract_get_fv3_regional_ensperts_class) :: get_fv3_regional_ensperts_class
   contains
     procedure, pass(this) :: get_fv3_regional_ensperts => get_fv3_regional_ensperts_run
@@ -8,109 +38,46 @@ use abstract_get_fv3_regional_ensperts_mod,only: abstract_get_fv3_regional_enspe
     procedure, pass(this) :: general_read_fv3_regional
   end type get_fv3_regional_ensperts_class
 
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 contains
-  subroutine get_fv3_regional_ensperts_run(this,en_perts,nelen,ps_bar)
-  !$$$  subprogram documentation block
-  !                .      .    .                                       .
-  ! subprogram:    get_fv3_regional_ensperts  read arw model ensemble members
-  !   prgmmr: Ting            org: EMC/NCEP            date: 2018-12-13
-  !
-  ! abstract: read ensemble members from the fv3 regional (fv3_SAR)
-  ! model,following Wanshu's programs to read those background files 
-  !
-  !
-  ! program history log:
-  !   2011-08-31  todling - revisit en_perts (single-prec) in light of extended bundle
-  !
-  !   input argument list:
-  !
-  !   output argument list:
-  !
-  ! attributes:
-  !   language: f90
-  !   machine: 
-  !
-  !$$$ end documentation block
-  
-      use kinds, only: i_kind,r_single
-      use gsi_bundlemod, only: gsi_bundle
-      implicit none
-      class(get_fv3_regional_ensperts_class)     , intent(inout):: this
-      type(gsi_bundle),allocatable               , intent(inout):: en_perts(:,:)
-      integer(i_kind)                            , intent(in   ):: nelen
-      real(r_single),dimension(:,:,:),allocatable, intent(inout):: ps_bar
+subroutine get_fv3_regional_ensperts_run(this,en_perts,nelen,ps_bar)
+    use kinds, only: i_kind,r_single
+    use gsi_bundlemod, only: gsi_bundle
+    implicit none
+
+  ! Declare passed variables
+    class(get_fv3_regional_ensperts_class)     , intent(inout):: this
+    type(gsi_bundle),allocatable               , intent(inout):: en_perts(:,:)
+    integer(i_kind)                            , intent(in   ):: nelen
+    real(r_single),dimension(:,:,:),allocatable, intent(inout):: ps_bar
   
     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
     end associate
   return
   end subroutine get_fv3_regional_ensperts_run
   
-  subroutine general_read_fv3_regional(this,fv3_filenameginput,g_ps,g_u,g_v,g_tv,g_rh,g_oz)
-  !$$$  subprogram documentation block
-  !     first compied from general_read_arw_regional           .      .    .                                       .
-  ! subprogram:    general_read_fv3_regional  read fv3sar model ensemble members
-  !   prgmmr: Ting             org: emc/ncep            date: 2018
-  !
-  ! abstract: read ensemble members from the fv3sar model in "restart" or "cold start"  netcdf format
-  !           for use with hybrid ensemble option. 
-  !
-  ! program history log:
-  !   2018-  Ting      - intial versions  
-  !
-  !   input argument list:
-  !
-  !   output argument list:
-  !
-  ! attributes:
-  !   language: f90
-  !   machine:  ibm RS/6000 SP
-  !
-  !$$$ end documentation block
+subroutine general_read_fv3_regional(this,fv3_filenameginput,g_ps,g_u,g_v,g_tv,g_rh,g_oz)
+    use kinds, only: r_kind
+    use gsi_rfv3io_mod,only: type_fv3regfilenameg
+    implicit none
   
-      use kinds, only: r_kind
-      use gsi_rfv3io_mod,only: type_fv3regfilenameg
-      implicit none
-  !
   ! Declare passed variables
-      class(get_fv3_regional_ensperts_class), intent(inout) :: this
-      type (type_fv3regfilenameg)           , intent(in)    :: fv3_filenameginput
-      real(r_kind),dimension(:,:,:),intent(out)::g_u,g_v,g_tv,g_rh,g_oz
-      real(r_kind),dimension(:,:  ),intent(out)::g_ps
+    class(get_fv3_regional_ensperts_class), intent(inout):: this
+    type (type_fv3regfilenameg)           , intent(in   ):: fv3_filenameginput
+    real(r_kind),dimension(:,:,:)         , intent(  out):: g_u,g_v,g_tv,g_rh,g_oz
+    real(r_kind),dimension(:,:  )         , intent(  out):: g_ps
 
     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
     end associate
   return
   end subroutine general_read_fv3_regional
-  subroutine ens_spread_dualres_regional_fv3_regional(this,mype,en_perts,nelen,en_bar)
-  !$$$  subprogram documentation block
-  !                .      .    .                                       .
-  ! subprogram:    ens_spread_dualres_regional
-  !   prgmmr: mizzi            org: ncar/mmm            date: 2010-08-11
-  !
-  ! abstract:
-  !
-  !
-  ! program history log:
-  !   2010-08-11  parrish, initial documentation
-  !   2011-04-05  parrish - add pseudo-bundle capability
-  !   2011-08-31  todling - revisit en_perts (single-prec) in light of extended bundle
-  !
-  !   input argument list:
-  !     en_bar - ensemble mean
-  !      mype  - current processor number
-  !
-  !   output argument list:
-  !
-  ! attributes:
-  !   language: f90
-  !   machine:  ibm RS/6000 SP
-  !
-  !$$$ end documentation block
-  !
+
+subroutine ens_spread_dualres_regional_fv3_regional(this,mype,en_perts,nelen,en_bar)
     use kinds, only: i_kind
     use gsi_bundlemod, only: gsi_bundle
     implicit none
 
+  ! Declare passed variables
     class(get_fv3_regional_ensperts_class), intent(inout) :: this
     type(gsi_bundle),OPTIONAL,    intent(in):: en_bar
     integer(i_kind ),             intent(in):: mype
@@ -119,8 +86,7 @@ contains
   
     associate( this => this ) ! eliminates warning for unused dummy argument needed for binding
     end associate
- 
-    return
+  return
   end subroutine ens_spread_dualres_regional_fv3_regional
   
 end module get_fv3_regional_ensperts_mod
