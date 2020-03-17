ccca2b50a6e987ff5a5521611f86b1772787ba77
Ting-Chi Wu <ting-chi.wu@noaa.gov>
2020-03-17 22:47:13 +0000

    update calibration difference values

--- src/gsi/setuprad.f90
+++ src/gsi/setuprad.f90
@@ -1219,12 +1219,6 @@ contains
 ! TCW 10/24/2019: account for calibration diff (NOAA-19 MHS - TEMPEST-D) prior to
 !                 using NOAA-19 MHS air-mass (and angle?) bias correction coefficient
            if (tempest) then
-!              tempest2mhs_calidiff(1) = -0.39
-!              tempest2mhs_calidiff(2) = -2.07
-!              tempest2mhs_calidiff(3) = -0.12
-!              tempest2mhs_calidiff(4) = -0.61
-!              tempest2mhs_calidiff(5) = -0.82
-
 ! TCW 02/27/2020: this is the very first attempt (no account for calibration diff)
 !              tempest2mhs_calidiff(1) = 0.0
 !              tempest2mhs_calidiff(2) = 0.0
@@ -1238,6 +1232,12 @@ contains
               tempest2mhs_calidiff(3) =  0.61
               tempest2mhs_calidiff(4) = -0.29
               tempest2mhs_calidiff(5) = -0.72
+! TCW 03/17/2020: use updated mean calibration differences (Table 3 in Berg et al, 2020) 
+              tempest2mhs_calidiff(1) = -0.40
+              tempest2mhs_calidiff(2) = -1.46
+              tempest2mhs_calidiff(3) = -0.69
+              tempest2mhs_calidiff(4) = -0.22
+              tempest2mhs_calidiff(5) =  0.88
 
               tbc(i) = tbc(i) - tempest2mhs_calidiff(i)*air_rad(mm)
            endif
