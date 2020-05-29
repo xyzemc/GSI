880a122a969767a5849f93fa47e1e739561795d0
andrew.eichmann <andrew.eichmann@noaa.gov>
2020-05-29 19:29:47 +0000

    bugfix provided by CWB

--- util/EFSOI_Utilities/src/scatter_chunks_efsoi.f90
+++ util/EFSOI_Utilities/src/scatter_chunks_efsoi.f90
@@ -18,7 +18,8 @@ contains
 subroutine scatter_chunks_ob_impact
 ! distribute chunks from grdin (read in controlvec) according to
 ! decomposition from load_balance
-use statevec_efsoi, only: grdin,grdin2,grdin3
+! AFE use statevec_efsoi, only: grdin,grdin2,grdin3
+use statevec_efsoi, only: grdin,grdin3,grdin5
 use gridinfo_efsoi, only: ncdim
 implicit none
 
@@ -112,7 +113,8 @@ if(nproc == 0) then
       do nn=1,ncdim
          do i=1,numptsperproc(np)
             n = ((np-1)*ncdim + (nn-1))*npts_max + i
-            sendbuf(n) = grdin2(indxproc(np,i),nn)
+! AFE           sendbuf(n) = grdin2(indxproc(np,i),nn)
+            sendbuf(n) = grdin5(indxproc(np,i),nn)
             sendbuf2(n) = grdin3(indxproc(np,i),nn)
          end do
       end do
@@ -138,7 +140,8 @@ end if
       end do
    end do
    deallocate(sendbuf,recvbuf,sendbuf2,recvbuf2)
-   if(allocated(grdin2)) deallocate(grdin2)
+! AFE   if(allocated(grdin2)) deallocate(grdin2)
+   if(allocated(grdin5)) deallocate(grdin5)
    call divide_weight(analmean_chunk)
    call divide_weight(ensmean_chunk(:,:))
 
